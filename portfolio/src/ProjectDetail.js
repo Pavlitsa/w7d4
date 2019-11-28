c4e6f1f83e6ec97e38ac000efc8635bb2baf8316
Pavlitsa <pzafeiri@gmail.com>
2019-11-28 15:46:45 +0100

    example

---
+++ portfolio/src/ProjectDetail.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { myProjects } from "./Projects";
+import { Link } from "react-router-dom";
+
+const ProjectDetail = props => {
+  const projectId = props.match.params.id;
+
+  const project = myProjects.find(el => {
+    return el.id === projectId;
+  });
+
+  console.log(projectId, project);
+
+  return (
+    <div>
+      <h2>{project.name}</h2>
+      <h3>Used technologies: {project.technologies}</h3>
+      <p>{project.description}</p>
+      <Link to="/projects">Back to the projects</Link>
+    </div>
+  );
+};
+
+export default ProjectDetail;
