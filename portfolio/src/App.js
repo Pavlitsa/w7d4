38cfe259229bba1544a03ec617b8e347d4c7c514
mjarraya <mjarraya@student.42.fr>
2019-11-28 11:19:41 +0100

    routing setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Home";
+import "./App.css";
+import About from "./About";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+      <About />
+    </div>
+  );
+}
+
+export default App;
