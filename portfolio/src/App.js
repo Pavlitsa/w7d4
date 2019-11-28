96466872d7b5a1c32cbba33a3b07e47c52dbd748
mjarraya <mjarraya@student.42.fr>
2019-11-28 14:45:34 +0100

    routing

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,12 +2,25 @@ import React from "react";
 import Home from "./Home";
 import "./App.css";
 import About from "./About";
+import { Route, Switch } from "react-router-dom";
+import Navbar from "./Navbar";
+
+const NotFound = () => {
+  return <div>404 not found</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <Home />
-      <About />
+      {/* <Home />
+      <About /> */}
+      <Navbar />
+      {/* Switch will only render one route, the first route that matches the path */}
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/about" component={About} />
+        <Route component={NotFound} />
+      </Switch>
     </div>
   );
 }
